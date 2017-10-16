8a842f8ba87a94ede1e5773fd82b11bfecab1515
Michael Jerred <mjerred.work@gmail.com>
2017-10-16 12:57:15 +0100

    Fixes

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const compression = require('compression');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(compression());
+app.use(express.static(__dirname + '/../dist'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../dist/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
