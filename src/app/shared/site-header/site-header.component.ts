3c90cf4eabef180967148ecef6fe54f4edb90ec8
Michael Jerred <mjerred.work@gmail.com>
2017-12-10 22:37:15 +0000

    Build fix

--- src/app/shared/site-header/site-header.component.ts
+++ src/app/shared/site-header/site-header.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 export class SiteHeaderComponent {
     constructor(
         @Inject('Window') private window: Window,
-        private router: Router) {
+        public router: Router) {
     }
 
     isScrolled = false;
