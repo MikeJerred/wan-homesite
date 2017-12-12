247f66ec844cfb8dd713df7c5b4a7bbbd9512882
Michael Jerred <mjerred.work@gmail.com>
2017-12-12 23:14:22 +0000

    Work on iamge gallery

--- src/app/photography/gallery/gallery.component.ts
+++ src/app/photography/gallery/gallery.component.ts
@@ -1,13 +1,61 @@
-import { Component } from '@angular/core';
-import { transition } from '@angular/animations';
+import { Component, Input } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
+    selector: 'app-gallery',
     templateUrl: './gallery.component.html',
     styleUrls: ['./gallery.component.scss'],
     animations: [
-
+        trigger('image', [
+            state('previous', style({ opacity: 0 })),
+            state('current', style({ opacity: 1 })),
+            state('next', style({ opacity: 0 })),
+            transition('previous <=> current', animate('1s ease-in-out')),
+            transition('next <=> current', animate('1s ease-in-out'))
+        ])
     ]
 })
 export class GalleryComponent {
+    //@Input('images') images: string[];
+    private images = [
+        '/assets/photographs/architecture/1.jpg',
+        '/assets/photographs/architecture/2.jpg',
+        '/assets/photographs/architecture/3.jpg',
+        '/assets/photographs/architecture/4.jpg'
+    ];
 
+    public currentIndex = 0;
+
+    public previous(): void {
+        if (this.currentIndex === 0)
+            this.currentIndex = this.images.length - 1;
+        else
+            this.currentIndex--;
+    }
+
+    public next(): void {
+        if (this.currentIndex >= this.images.length - 1)
+            this.currentIndex = 0;
+        else
+            this.currentIndex++;
+    }
+
+    public getImageState(index: number): string {
+        if (index === this.currentIndex)
+            return 'current';
+
+        const previousIndex = this.currentIndex === 0
+            ? this.images.length - 1
+            : this.currentIndex - 1;
+        if (index === previousIndex)
+            return 'previous';
+
+        const nextIndex = this.currentIndex >= this.images.length - 1
+            ? 0
+            : this.currentIndex + 1;
+        if (index === nextIndex)
+            return 'next';
+
+        return 'void';
+    }
 }
--- src/app/photography/photography.module.ts
+++ src/app/photography/photography.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
+import { ModalModule } from 'ngx-bootstrap';
 
 import { SharedModule } from 'shared/shared.module';
 import { PhotographyRoutingModule } from './photography-routing.module';
 import { ArchitecturePortfolioComponent } from './architecture/architecture-portfolio.component';
 import { StreetPortfolioComponent } from './street/street-portfolio.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
     imports: [
+        ModalModule.forRoot(),
         SharedModule,
         PhotographyRoutingModule
     ],
     declarations: [
         ArchitecturePortfolioComponent,
-        StreetPortfolioComponent
+        StreetPortfolioComponent,
+        GalleryComponent
     ]
 })
 export class PhotographyModule {}
