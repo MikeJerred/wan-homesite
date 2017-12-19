9c2ee39d41341b1c61305e05dfd57b7df68d24a1
Michael Jerred <mjerred.work@gmail.com>
2017-12-19 17:34:56 +0000

    Reworknig page transitions : still not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { AnimationEntryMetadata, Component, OnInit } from '@angular/core';
 import { Location, PopStateEvent } from '@angular/common';
 import { NavigationStart, NavigationEnd, Router, RouterOutlet } from '@angular/router';
-import { animate, group, query, state, style, transition, trigger } from '@angular/animations';
+import { AnimationEvent, animate, group, query, state, style, transition, trigger } from '@angular/animations';
 import { PageScrollConfig } from 'ngx-page-scroll';
 import * as $ from 'jquery';
 
@@ -37,9 +37,9 @@ const homeJoin: AnimationEntryMetadata = [
 const slideLeft: AnimationEntryMetadata = [
     query(
         ':leave',
-        style({ position: 'absolute', top: 0, left: 0, width: '100vw', transform: 'translateX(0)' }),
+        style({ position: 'fixed', left: 0, width: '100vw', transform: 'translateX(0)' }),
         { optional: true }),
-    query(':enter', style({ position: 'fixed', left: 0, width: '100vw', transform: 'translateX(100vw)', 'z-index': 100 })),
+    query(':enter', style({ position: 'absolute', left: 0, width: '100vw', transform: 'translateX(100vw)', 'z-index': 100 })),
     group([
         query(':leave', animate('1s ease-in-out', style({ transform: 'translateX(-70vw)' })), { optional: true }),
         query(':enter', animate('1s ease-in-out', style({ transform: 'translateX(0)' }))),
@@ -49,12 +49,44 @@ const slideLeft: AnimationEntryMetadata = [
 const slideRight: AnimationEntryMetadata = [
     query(
         ':leave',
-        style({ position: 'absolute', top: 0, left: 0, width: '100vw', transform: 'translateX(0)' }),
+        style({ position: 'fixed', left: 0, width: '100vw', transform: 'translateX(0)' }),
         { optional: true }),
-    query(':enter', style({ position: 'fixed', left: 0, width: '100vw', transform: 'translateX(-100vw)', 'z-index': 100 })),
+    query(
+        ':enter',
+        style({ position: 'absolute', left: 0, width: '100vw', transform: 'translateX(-100vw)', 'z-index': 100 })),
     group([
         query(':leave', animate('1s ease-in-out', style({ transform: 'translateX(70vw)' })), { optional: true }),
         query(':enter', animate('1s ease-in-out', style({ transform: 'translateX(0)' }))),
+    ])
+];
+
+const slideLeftBothParallax: AnimationEntryMetadata = [
+    query(':leave > .parallax', style({ 'margin-left': 0 })),
+    query(':leave > .not-parallax', style({ position: 'fixed', left: 0, width: '100vw', transform: 'translateX(0)' })),
+    query(':enter > .parallax', style({ 'margin-left': '100vw', 'z-index': 99 })),
+    query(
+        ':enter > .not-parallax',
+        style({ position: 'absolute', left: 0, width: '100%', transform: 'translateX(100vw)', 'z-index': 100 })),
+    group([
+        query(':leave > .parallax', animate('1s ease-in-out', style({ 'margin-left': '-70vw' }))),
+        query(':leave > .not-parallax', animate('1s ease-in-out', style({ transform: 'translateX(-70vw)' }))),
+        query(':enter > .parallax', animate('1s ease-in-out', style({ 'margin-left': 0 }))),
+        query(':enter > .not-parallax', animate('1s ease-in-out', style({ transform: 'translateX(0)' }))),
+    ])
+];
+
+const slideRightBothParallax: AnimationEntryMetadata = [
+    query(':leave > .parallax', style({ transform: 'translateX(0)' })),
+    query(':leave > .not-parallax', style({ position: 'fixed', left: 0, width: '100vw', transform: 'translateX(0)' })),
+    query(':enter > .parallax', style({ transform: 'translateX(-100vw)', 'z-index': 99 })),
+    query(
+        ':enter > .not-parallax',
+        style({ position: 'absolute', left: 0, width: '100%', transform: 'translateX(-100vw)', 'z-index': 100 })),
+    group([
+        query(':leave > .parallax', animate('1s ease-in-out', style({ transform: 'translateX(70vw)' }))),
+        query(':leave > .not-parallax', animate('1s ease-in-out', style({ transform: 'translateX(70vw)' }))),
+        query(':enter > .parallax', animate('1s ease-in-out', style({ transform: 'translateX(0)' }))),
+        query(':enter > .not-parallax', animate('1s ease-in-out', style({ transform: 'translateX(0)' }))),
     ])
 ];
 
@@ -75,6 +107,7 @@ const slideRight: AnimationEntryMetadata = [
             transition('design-4 => design-2', slideLeft),
             transition('design-4 => design-3', slideLeft),
             transition('photography-street => photography-architecture', slideLeft),
+            transition('design-portfolio <=> photography-architecture', slideLeft),
             transition('* => *', slideRight)
         ])
     ]
@@ -83,6 +116,7 @@ export class AppComponent implements OnInit {
     private lastPoppedUrl: string;
     private previousUrl: string;
     private scrollYValues: { [key: string]: number } = {};
+    private animationDoneQueue: ((event: AnimationEvent) => void)[] = [];
 
     constructor(private router: Router, private location: Location) {
         PageScrollConfig.defaultDuration = 500;
@@ -107,28 +141,49 @@ export class AppComponent implements OnInit {
         this.router.events
             .filter(evt => evt instanceof NavigationEnd)
             .subscribe((evt: NavigationEnd) => {
+                const roots = $('app-root > main > ng-component');
+                const enter = roots.first();
+                const leave = roots.last();
+
+                leave.css({
+                    top: -this.scrollYValues[this.previousUrl] + 'px'
+                });
+
                 if (evt.url === this.lastPoppedUrl) {
                     this.lastPoppedUrl = undefined;
 
-                    // matches :enter
-                    const enterElement = $('app-root > main > ng-component').first();
-                    enterElement.css({
-                        top: -this.scrollYValues[evt.url] + 'px'
+                    window.scrollTo(0, this.scrollYValues[evt.url]);
+                    // enter.css({
+                    //     top: -this.scrollYValues[evt.url] + 'px'
+                    // });
+                    this.animationDoneQueue.push(() => {
+                        enter.css({ top: '' });
+                        //window.scrollTo(0, this.scrollYValues[evt.url]);
                     });
-
-                    setTimeout(
-                        () => {
-                            enterElement.css({ top: '' });
-                            window.scrollTo(0, this.scrollYValues[evt.url]);
-                        },
-                        1000);
                 } else if (evt.url !== '/home') {
-                    setTimeout(() => window.scrollTo(0, 0), 1000);
-                    console.log('Scrolling to top');
+                    window.scrollTo(0, 0);
+                    // enter.css({
+                    //     top: '0'
+                    // });
+
+                    this.animationDoneQueue.push(event => {
+                        enter.css({ top: '' });
+
+                        //if (event.fromState !== 'null')
+                            //window.scrollTo(0, 0);
+                    });
                 }
 
                 this.previousUrl = evt.url;
             });
+    }
+
+    public animationDone(event: AnimationEvent): void {
+        this.animationDoneQueue.forEach(func => {
+            func(event);
+        });
+
+        this.animationDoneQueue = [];
     }
 
     public getRouteTransition(outlet: RouterOutlet) {
