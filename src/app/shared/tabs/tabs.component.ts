33922e1359cc68790f67e83081bccb3de5e4f82c
Michael Jerred <mjerred.work@gmail.com>
2018-09-24 18:32:12 +0900

    Changing tab slide animation

--- src/app/shared/tabs/tabs.component.ts
+++ src/app/shared/tabs/tabs.component.ts
@@ -15,12 +15,12 @@ export const tabAnimation: AnimationEntryMetadata = [
                 animate('1s ease-in-out', style({ height: '{{newHeight}}' }))
             ]),
             query(':leave', [
-                style({ position: 'absolute', top: 0, left: 0, right: 0, transform: 'translateY(0)' }),
-                animate('1s ease-in-out', style({ transform: 'translateY(50vh)' }))
+                style({ position: 'absolute', top: 0, left: 0, right: 0, transform: 'translateX(0)', opacity: 1 }),
+                animate('1s ease-in-out', style({ transform: 'translateX({{slideOutSign}}50vw)', opacity: 0 }))
             ], { optional: true }),
             query(':enter', [
-                style({ position: 'absolute', top: 0, left: 0, right: 0, transform: 'translateY(-100vh)', 'z-index': 1 }),
-                animate('1s ease-in-out', style({ transform: 'translateY(0)' }))
+                style({ position: 'absolute', top: 0, left: 0, right: 0, transform: 'translateX({{slideInSign}}100vw)', 'z-index': 1 }),
+                animate('1s ease-in-out', style({ transform: 'translateX(0)' }))
             ])
         ])
     ])
@@ -39,6 +39,7 @@ export class TabsComponent implements AfterContentInit {
 
     public selectedTab = 0;
     public isMobile = false;
+    public previousTabIndex = 0;
     public previousHeight = '0';
     public currentHeight = '0';
 
@@ -62,6 +63,7 @@ export class TabsComponent implements AfterContentInit {
     }
 
     public selectTab(selectedTab: TabComponent) {
+        this.previousTabIndex = this.tabs.toArray().findIndex(tab => tab.active);
         this.tabs.forEach(tab => tab.active = false);
         selectedTab.active = true;
 
@@ -82,7 +84,12 @@ export class TabsComponent implements AfterContentInit {
 
         return {
             value: 'state' + index,
-            params: { oldHeight: this.previousHeight, newHeight: this.currentHeight }
+            params: {
+                oldHeight: this.previousHeight,
+                newHeight: this.currentHeight,
+                slideOutSign: index < this.previousTabIndex ? '-' : '',
+                slideInSign: index < this.previousTabIndex ? '' : '-'
+            }
         };
     }
 }
