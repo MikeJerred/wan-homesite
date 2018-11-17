a09f91b6a8676bb48ff8cf899f7389b75b71f05c
Michael Jerred <mjerred.work@gmail.com>
2018-11-17 22:22:15 +0000

    Fix

--- server/index.js
+++ server/index.js
@@ -5,10 +5,12 @@ const path = require('path');
 const app = express();
 
 app.use(compression());
-app.use(express.static(__dirname + '/../dist'));
+app.use(express.static(__dirname + '/../dist/wan-homesite'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../dist/index.html'));
+    res.sendFile(path.join(__dirname + '/../dist/wan-homesite/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+    console.log('Listening');
+});
