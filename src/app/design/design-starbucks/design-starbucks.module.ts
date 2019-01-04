f606db1a3c94b7fa8a9dc4de83172aedf0886209
Michael Jerred <mjerred.work@gmail.com>
2019-01-04 15:08:37 +0000

    Adding Starbucks tab

---
+++ src/app/design/design-starbucks/design-starbucks.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NgxPageScrollModule } from 'ngx-page-scroll';
+
+import { SharedModule } from '~/shared/shared.module';
+import { DesignStarbucksComponent } from './design-starbucks.component';
+import { TabResearchComponent } from './tab-research/tab-research.component';
+import { TabPrototypeComponent } from './tab-prototype/tab-prototype.component';
+import { TabDesignComponent } from './tab-design/tab-design.component';
+import { TabDemonstrationComponent } from './tab-demonstration/tab-demonstration.component';
+
+const routes: Routes = [
+    { path: '5', component: DesignStarbucksComponent }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(routes),
+        NgxPageScrollModule,
+
+        SharedModule
+    ],
+    declarations: [
+        DesignStarbucksComponent,
+        TabResearchComponent,
+        TabPrototypeComponent,
+        TabDesignComponent,
+        TabDemonstrationComponent
+    ],
+    exports: [ RouterModule ]
+})
+export class DesignStarbucksModule {}
